a1bf43fce76fa543e89d8b79e876447cf2aa7b1c
Poongothai Senthurkumar <sujapoo@gmail.com>
2026-03-16 18:07:01 +1100

    Fix ESLint errors for Vercel build

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -54,7 +54,7 @@ const DashboardComponent = () => {
       await fetchStudents();
     };
     check();
-  }, []);
+  }, [navigate]);
 
   const fetchStudents = async () => {
     try {
--- src/Services/AuthService.js
+++ src/Services/AuthService.js
@@ -61,4 +61,5 @@ class AuthService {
   }
 }
 
-export default new AuthService();
+const authService = new AuthService();
+export default authService;
